616f7ad9a928cb5313fa71221b0120a67d650d86
assignment <prakitz2908@gmail.com>
2023-06-23 11:06:16 +0530

    added Error Handling

--- seeds/index.js
+++ seeds/index.js
@@ -1,30 +1,36 @@
-const mongoose = require("mongoose");
-const Campground = require("../models/campground");
-mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp");
-const cities = require("./cities");
-const { places, descriptors } = require("./seedHelpers");
+const mongoose = require('mongoose');
+const cities = require('./cities');
+const { places, descriptors } = require('./seedHelpers');
+const Campground = require('../models/campground');
+
+mongoose.connect('mongodb://127.0.0.1:27017/yelp-camp');
+
 const db = mongoose.connection;
+
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log("DataBase Connected");
+    console.log("Database connected");
 });
-const price = Math.floor(Math.random()*20)+10;
-const sample = (array) => array[Math.floor(Math.random() * array.length)];
-const seeDB = async () => {
-  await Campground.deleteMany({});
-  for (let i = 0; i < 50; i++) {
-    const random1000 = Math.floor(Math.random() * 1000);
-    const camp = new Campground({
-      location: `${cities[random1000].city},${cities[random1000].state}`,
-      title: `${sample(descriptors)} ${sample(places)}`,
-      image: 'https://source.unsplash.com/collection/9407462',
-      description:
-        "    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam voluptates odit voluptatum, dicta exercitationem quae expedita commodi eligendi ipsa tempora libero, eaque fuga veritatis repellat temporibus iusto necessitatibus fugiat a!",
-        price
-    });
-    await camp.save();
-  }
-};
-seeDB().then(() => {
-  mongoose.connection.close();
-});
+
+const sample = array => array[Math.floor(Math.random() * array.length)];
+
+
+const seedDB = async () => {
+    await Campground.deleteMany({});
+    for (let i = 0; i < 50; i++) {
+        const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
+        const camp = new Campground({
+            location: `${cities[random1000].city}, ${cities[random1000].state}`,
+            title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251',
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam dolores vero perferendis laudantium, consequuntur voluptatibus nulla architecto, sit soluta esse iure sed labore ipsam a cum nihil atque molestiae deserunt!',
+            price
+        })
+        await camp.save();
+    }
+}
+
+seedDB().then(() => {
+    mongoose.connection.close();
+})
