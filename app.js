ffd04f5c08bab7d9673eea4c299ed1a30cc7dfe8
assignment <prakitz2908@gmail.com>
2023-06-28 00:05:56 +0530

    added login and register

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const path = require("path");
 const mongoose = require("mongoose");
 const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
-const campgrounds = require("./routes/campgrounds");
-const reviews = require("./routes/reviews");
 const ExpressError = require("./utils/ExpressError");
 const session = require("express-session");
-const flash = require('connect-flash')
+const flash = require("connect-flash");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
+
+const campgroundRoutes = require("./routes/campgrounds");
+const reviewRoutes = require("./routes/reviews");
+const userRoutes = require("./routes/user");
+
 mongoose.connect("mongodb://127.0.0.1:27017/yelp-camp", {
   useUnifiedTopology: true,
   useNewUrlParser: true,
@@ -33,23 +39,29 @@ const sessionConfig = {
   secret: "thisshouldbeabettersecret",
   resave: false,
   saveUninitialized: true,
-  cookie :{
-    httpOnly:true,
-    expires: Date.now()+1000*60*60*24*7,
-    maxAge :1000*60*60*24*7
-  }
+  cookie: {
+    httpOnly: true,
+    expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
+    maxAge: 1000 * 60 * 60 * 24 * 7,
+  },
 };
-app.use(session(sessionConfig))
+app.use(session(sessionConfig));
 app.use(flash());
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
-app.use((req,res,next)=>{
-  res.locals.success = req.flash('success')
-  res.locals.error = req.flash('error')
+app.use((req, res, next) => {
+  res.locals.success = req.flash("success");
+  res.locals.error = req.flash("error");
   next();
-})
+});
 
-app.use("/campgrounds", campgrounds);
-app.use("/campgrounds/:id/reviews", reviews);
+app.use("/", userRoutes);
+app.use("/campgrounds", campgroundRoutes);
+app.use("/campgrounds/:id/reviews", reviewRoutes);
 
 app.get("/", (req, res) => {
   res.render("home");
